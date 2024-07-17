ef82649ef9e5a45dadd100fb83137b6a6e8d3e03
arisha <arishanawaz2002@gmail.com>
2024-07-18 02:26:21 +0500

    add

--- MapProject/map-project/src/components/Countrieslistofcountries.jsx
+++ MapProject/map-project/src/components/Countrieslistofcountries.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import  {countriesData} from '../data';
+import  {countriesData} from '../api/data';
 import CountriesCard from "./CountriesCard";
 
 function CountriesList({ query, selectedRegion }) {
--- MapProject/map-project/src/main.jsx
+++ MapProject/map-project/src/main.jsx
@@ -10,3 +10,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+//statehook
+//list
+//
