8568989293860f9cd7ea0dd528186726a8b746ac
arisha <arishanawaz2002@gmail.com>
2024-07-20 15:21:54 +0500

    ADD

--- Meet-Display/Meet-Display/src/components/Meet/ParticipantList.jsx
+++ Meet-Display/Meet-Display/src/components/Meet/ParticipantList.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ParticipantCard from './ParticipantCard';
 import './ParticipantList.css';
 
-function ParticipantList({ participantList=[] }) {
+function ParticipantList({ participantList = [] }) {
   return (
     <div className="participant-list">
       {participantList.map((participant, index) => (
-        <ParticipantCard
+        <ParticipantCard 
           key={index}
-          participantName={participant.name}
-          participantImage={participant.image}
-          participantRole={participant.role}
         />
       ))}
     </div>
